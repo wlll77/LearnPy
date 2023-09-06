ed05c53c1e528ed133bc1789e7222be75a4c70d8
JS-WL <wlll77@qq.com>
2023-09-06 13:53:00 +0800

    更新代码测试

---
+++ if learn.py
@@ -0,0 +1,15 @@
+#if
+# if True:
+#     print("执行此代码")
+
+#if 条件：
+#   可以是单行或多行执行语句
+
+# num1 = 10
+# num2 = 5
+# if num1 > num2:
+#     print("num1 > num2")
+#     num1 += 1
+#     print(num1)
+
+#elif (else if的缩写） 即再如果
